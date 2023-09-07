5ae60ed9d181aa490e74752f89dc5faf2ba8dde5
permCoding <ttxiom@gmail.com>
2023-09-07 13:50:04 +0500

    lect01

---
+++ part-1/lections/lect01/p01.py
@@ -0,0 +1,14 @@
+def get_double(num):
+    """ увеличить в два раза """
+    return num + num 
+
+
+def get_pow(num, p):
+    pass
+    x = 0
+    return num**p
+
+
+num = int(input("Введите число - "))
+print(f'num = {get_double(num)}')
+
