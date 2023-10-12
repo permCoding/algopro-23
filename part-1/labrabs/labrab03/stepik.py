fed61e808f95ee21ea13282495d300c9e525d6f9
permCoding <ttxiom@gmail.com>
2023-10-12 14:51:53 +0500

    map

---
+++ part-1/labrabs/labrab03/stepik.py
@@ -0,0 +1,12 @@
+x = 123
+
+# print(x%2 == 0)  # True
+
+# if x%2 == 2:
+#     print("True")
+# else:
+#     print("False")
+
+s = "12 34"
+print(max(map(int, input().split())))
+# print(max([int(elm) for elm in input().split()]))
