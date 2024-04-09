a50254a5bdbd83ece3b5c5594a4ee0c577f56c08
Андрей Беляков <ttxiom@gmail.com>
2024-04-09 08:47:39 +0500

    labrab-05

--- part-2/lections/lect-09/module_sorting.py
+++ part-2/lections/lect-09/module_sorting.py
@@ -17,17 +17,19 @@ def get_sorted_list(nums):
 def get_sorted_list_opt(nums):
     """
     вернуть отсортированный список (методом выбора)  
-    с оптимизацией - если не было ни однойзамены  
+    с оптимизацией - если при поиске выбора
+    оказалось что все упорядочены
     то дальше прекратить сортировку  
     """
     for left in range(0, len(nums)-1):
-        # b = True
+        b = True
         pos_min = left
         for pos_cur in range(left, len(nums)):
-            # if nums[pos_cur] < nums[pos_cur-1]: b = False
+            if left > 0 and nums[pos_cur] < nums[pos_cur-1]: 
+                b = False
             if nums[pos_cur] < nums[pos_min]:
                 pos_min = pos_cur
         
         nums[pos_min], nums[left] = nums[left], nums[pos_min]
-        # if b == True: return nums
+        if left > 0 and b == True: return nums
         print(nums)
